ee14ac040952a11eacf9277dcd9b32fe7cae03c8
loochlabs <thecalooch@gmail.com>
2025-09-24 06:40:50 +0200

    fixed editor bug, level deisgn

--- run.bat
+++ run.bat
@@ -1,2 +1,15 @@
-jai .\build.jai
+@echo off
+setlocal
+
+set PARAMS=%*
+
+jai .\build.jai - %PARAMS%
+
+if %errorlevel% neq 0 (
+	echo Compilation failed
+	exit /b %errorlevel%
+)
+
 .\GameF.exe
+
+endlocal
